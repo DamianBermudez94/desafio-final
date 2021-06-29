4156af9f4bcd4ad595c5cfe19b609f80d67b046c
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2021-06-28 23:57:33 -0300

    first commit

---
+++ contacto/contacto.js
@@ -0,0 +1,64 @@
+function contactComponet(el) {
+  const componentEl = document.createElement("div");
+  componentEl.innerHTML = ` <section class="container__formulario">
+    <<h2 class="container__formulario-title">Escribime</h2>
+    <form class="container__form" action="">
+        <label for="">
+            <p class="form-title">Nombre</p>
+            <input id="nombre" class="label-input" type="text">
+        </label>
+        <label for="">
+            <p class="form-title">Email</p>
+            <input id="email" class="label-input" type="text">
+        </label>
+        <label for="">
+            <p class="form-title">Nombre</p>
+            <textarea id="textarea" class="label-input textarea" name="" id=""></textarea>
+        </label>
+        <div class="container__button-enviar">
+            <button class="btn-enviar">Enviar</button>
+        </div>
+    </form>
+  </section>
+     `;
+
+  el.appendChild(componentEl);
+}
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+function contacto() {
+  const form = document.querySelector(".container__form");
+  form.addEventListener("submit", (res) => {
+    res.preventDefault();
+    let objetoForm = res.target;
+    let nombre = objetoForm.nombre.value;
+    let email = objetoForm.email.value;
+    let textarea = objetoForm.textarea.value;
+
+    let mensaje = `nombre: ${nombre} 
+        email: ${email}
+        mensaje:${textarea}`;
+
+    let datos = {
+      to: "bermudezdamian7@gmail.com",
+      message: mensaje,
+    };
+    postData("https://apx-api.vercel.app/api/utils/dwf", datos).then((data) => {
+      console.log(data);
+    });
+  });
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+// Animacion del menu
+let btnMenu = document.querySelector("#btn-menu");
+let menu = document.querySelector(".container__menu");
+
+let activador = true;
+
+btnMenu.addEventListener("click", function () {
+  document.querySelector("#btn-menu i").classList.toggle("fa-times");
+  if (activador) {
+    menu.style.left = "0";
+    menu.style.transition = "0.6s";
+    activador = false;
+  } else {
+    activador = false;
+    menu.style.left = "-100%";
+    menu.style.transition = "0.6s";
+    activador = true;
+  }
+});
+function main() {
+  contactComponet(document.querySelector(".form-container"));
+  contacto();
+}
+main();
